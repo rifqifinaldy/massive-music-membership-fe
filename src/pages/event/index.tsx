4afbb19c22201d5f9b5b2b05b85ebeae1d7ad3dc
Rifqi Finaldy <finaldyrifqi@gmail.com>
2024-11-28 00:25:35 +0700

    feat(layout): added coming soon components and not found pages

---
+++ src/pages/event/index.tsx
@@ -0,0 +1,9 @@
+import ComingSoon from "@MME-components/coming-soon";
+import { NextPage } from "next";
+import React from "react";
+
+const EventPages: NextPage = () => {
+  return <ComingSoon />;
+};
+
+export default EventPages;
