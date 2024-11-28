69de30182253aaa7333a6ce89037d36619d77738
Rifqi Finaldy <rifqifinaldy@gmail.com>
2024-11-28 17:33:10 +0700

    feat(homepage): initiated simple homepage ui's

---
+++ src/app/components/cards/dashboard-module-card.tsx
@@ -0,0 +1,45 @@
+import { Button, Flex, Text } from "@chakra-ui/react";
+import React from "react";
+import { BORDER, RADIUS } from "../../../themes/variant";
+
+interface IProps {
+  icon: React.JSX.Element;
+  moduleName: string;
+  buttonText: string;
+  buttonClick: () => void;
+  description: string;
+}
+
+const DashboardModuleCard: React.FC<IProps> = ({
+  icon,
+  moduleName,
+  buttonClick,
+  buttonText,
+  description,
+}) => {
+  return (
+    <Flex
+      w={{ base: "400px", xl: "480px" }}
+      h="380px"
+      rounded={RADIUS.SM}
+      border={BORDER.GREEN}
+      flexDir="column"
+      justifyContent="center"
+      alignItems="center"
+      gap="20px"
+    >
+      {icon}
+      <Text textAlign="center" fontSize="28px">
+        {moduleName}
+      </Text>
+      <Text textAlign="center" fontSize="14px">
+        {description}
+      </Text>
+      <Button size="sm" onClick={() => buttonClick()}>
+        {buttonText}
+      </Button>
+    </Flex>
+  );
+};
+
+export default DashboardModuleCard;
--- src/app/pages-modules/homepage/index.tsx
+++ src/app/pages-modules/homepage/index.tsx
@@ -1,7 +1,38 @@
+import { Flex } from "@chakra-ui/react";
+import DashboardModuleCard from "@MME-components/cards/dashboard-module-card";
+import { NAVIGATION } from "@MME-config/navigation-menu";
+import { useRouter } from "next/router";
 import React from "react";
+import { AiFillCalendar, AiOutlineUser } from "react-icons/ai";
 
 const Homepage = () => {
-  return <div>Homepage</div>;
+  const router = useRouter();
+
+  return (
+    <Flex
+      alignItems="center"
+      w="full"
+      gap="80px"
+      h="full"
+      justifyContent="center"
+      flexDir={{ lg: "row", base: "column" }}
+    >
+      <DashboardModuleCard
+        moduleName="Manage Member"
+        icon={<AiOutlineUser size="80px" />}
+        description="Update existing member information / Add new member"
+        buttonClick={() => router.push(NAVIGATION.MEMBERSHIP)}
+        buttonText="Go to Member Area"
+      />
+      <DashboardModuleCard
+        moduleName="Manage Event"
+        icon={<AiFillCalendar size="80px" />}
+        description="Create / Update an Scheduled event for the organization"
+        buttonClick={() => router.push(NAVIGATION.EVENT)}
+        buttonText="Go to Event Area"
+      />
+    </Flex>
+  );
 };
 
 export default Homepage;
